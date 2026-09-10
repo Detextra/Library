28d121fb882479066c8ec695bf8925b23f474675
Antonin Boyon <antonin.boyon@gmail.com>
2026-09-10 09:56:47 +0200

    adding controller test

---
+++ TestProject1/LoanControllerTests.cs
@@ -0,0 +1,19 @@
+﻿using Library;
+using NSubstitute;
+namespace LibraryTests
+{
+    [TestFixture]
+    internal class LoanControllerTests
+    {
+        [Test]
+        public void MakeLoan_ShouldDelegateToLibraryManager()
+        {
+            var mockManager = Substitute.For<LibraryManager>();
+            var controller = new LoanController(mockManager);
+
+            controller.MakeLoan(bookId: 10, memberId: 5);
+
+            mockManager.Received(1).MakeLoan(10, 5);
+        }
+    }
+}
